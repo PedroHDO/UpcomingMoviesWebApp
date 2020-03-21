9b960ecae6c18362fb96d07d972742b48c8a0a83
PedroHDO <phdo.pedro@gmail.com>
2020-03-21 20:42:10 -0300

    Add Common Project

---
+++ backend/UpcomingMovies.Common/Http/Connection/HttpClientWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UpcomingMovies.Common.Http.Connection
+{
+    public class HttpClientWrapper : IHttpClientWrapper
+    {
+        private readonly HttpClient httpClient;
+
+        public HttpClientWrapper(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public string ApiToken { get; set; }
+
+        public string BaseAddress
+        {
+            get => httpClient.BaseAddress.ToString();
+            set => httpClient.BaseAddress = new Uri(value);
+        }
+        private string GetApiTokenParameter => $"api_key={ApiToken}";
+
+        public async Task<Stream> GetStreamAsync(string path)
+        {
+            return await httpClient.GetStreamAsync(path);
+        }
+
+        public async Task<string> GetStringAsync(string endpoint = "", string parameters = "")
+        {
+            var requestUri = $"{endpoint}?{GetApiTokenParameter}";
+
+            if (!string.IsNullOrEmpty(parameters))
+                requestUri += $"&{parameters}";
+
+            return await httpClient.GetStringAsync(requestUri);
+        }
+    }
+}
---
+++ backend/UpcomingMovies.Common/Http/Connection/IHttpClientWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace UpcomingMovies.Common.Http.Connection
+{
+    public interface IHttpClientWrapper
+    {
+        string ApiToken { get; set; }
+
+        string BaseAddress { get; set; }
+
+        Task<string> GetStringAsync(string path = "", string parameters = "");
+
+        Task<Stream> GetStreamAsync(string path);
+    }
+}
