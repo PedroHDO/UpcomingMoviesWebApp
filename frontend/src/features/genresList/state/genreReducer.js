50537f87713c454ab2fdb62abda4d3cc69734383
PedroHDO <phdo.pedro@gmail.com>
2020-03-22 12:11:40 -0300

    Change genre reducer to use switch

--- frontend/src/features/genresList/state/genreReducer.js
+++ frontend/src/features/genresList/state/genreReducer.js
@@ -2,16 +2,16 @@ import initialState from './initialState';
 import * as types from './actionTypes';
 
 export default (state = initialState, action) => {
-    if (action.type === types.SET_IS_LOADING_GENRES) {
-        return { ...state, isLoadingGenres: action.payload };
-    }
-
-    if (action.type === types.SET_GENRES) {
-        const genres = action.payload;
-        if (genres) {
-            return { ...state, genres };
+    switch (action.type) {
+        case types.SET_GENRES : {
+            const genres = action.payload;
+            if (genres) {
+                return { ...state, genres };
+            }
+            break;
         }
+        default: return state;
     }
-
+    
     return state;
 }
