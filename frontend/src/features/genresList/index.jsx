a75e229e5e9ea20cddf99a72de085ffd90fcf79c
PedroHDO <phdo.pedro@gmail.com>
2020-03-21 23:07:40 -0300

    refact GenresList

--- frontend/src/features/genresList/index.jsx
+++ frontend/src/features/genresList/index.jsx
@@ -2,24 +2,25 @@ import  React from 'react';
 import { connect } from 'react-redux';
 import  { getGenresById } from './state/selector';
 
-const GenreList = ({genres, genreIds, isLoading}) => {   
-    
+const GenreList = ({ genres, genreIds }) => {
     function getGenreName(genreId) {
         const genre = getGenresById(genres, genreId);
         return genre.name;
     }
-    return (
-      <span>
-        {genreIds && genreIds.map(getGenreName).join(', ')}
-        {!genreIds && "Genre not informed."}
-      </span>
-      );
+
+    if (!genreIds) {
+        return (<span>Genre not informed.</span>);
+    }
+
+    const formattedGenres = genreIds.map(getGenreName).join(', ');
+
+    return (<span>{formattedGenres}</span>);
 }
   const mapStateToProps = (state, ownProps) => {          
     const isLoading = state.genre.isLoadingGenres;
     return { 
         genres: state.genre.genres,
-        genreIds : ownProps.genreIds, 
+        genreIds: ownProps.genreIds, 
         isLoading
     }
   }
