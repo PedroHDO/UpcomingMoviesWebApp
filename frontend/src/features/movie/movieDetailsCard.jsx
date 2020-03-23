e06d1c98902c55f5e9f0c71aaa077859bc91ad2f
PedroHDO <phdo.pedro@gmail.com>
2020-03-23 00:22:52 -0300

    remove useGenres

--- frontend/src/features/movie/movieDetailsCard.jsx
+++ frontend/src/features/movie/movieDetailsCard.jsx
@@ -1,16 +1,9 @@
 import React, {useEffect} from 'react';
 import MovieImage from './movieImage';
 import GenreList from '../genres/list';
-import useGenres from '../genres/useGenres';
 import DateFormater from '../../common/dateFormater';
 
 const MovieDetailsCard = ({movie}) => {
-    const { fetchGenres } = useGenres();
-    
-    useEffect(() => {
-        fetchGenres();        
-    }, []);  
-
     return (
         <div className='row'>
             <div className='col-md-6'>
