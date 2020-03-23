7b96ec1fafa2506a57af33798308ac9142c2b71a
PedroHDO <phdo.pedro@gmail.com>
2020-03-23 00:12:02 -0300

    include handleScroll in useEffect InfiniteScroll

--- frontend/src/common/infiniteScroll.js
+++ frontend/src/common/infiniteScroll.js
@@ -4,18 +4,20 @@ const useInfiniteScroll = () => {
     const [isEndOfPage, setIsEndOfPage] = useState(false);
 
     useEffect(() => {
+        function handleScroll() {        
+            if (isEndOfPage) return;
+    
+            if (window.innerHeight + document.documentElement.scrollTop + 200 
+                < document.documentElement.offsetHeight) return;
+            
+            setIsEndOfPage(true);
+        }
+        
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-    function handleScroll() {        
-        if (isEndOfPage) return;
-
-        if (window.innerHeight + document.documentElement.scrollTop + 200 
-            < document.documentElement.offsetHeight) return;
-        
-        setIsEndOfPage(true);
-    }
+    
 
     function eventWasTrigger() {
         setIsEndOfPage(false);
